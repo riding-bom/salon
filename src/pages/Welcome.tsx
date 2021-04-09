1aa28a67150cbbc3d03bc10c87f42f78fe41ca02
yhun940731@gmail.com <yhun940731@gmail.com>
2021-04-09 17:15:43 +0900

    [수정] Welcome 내부 테스팅 코드 정리 (closed #2)

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,18 +1,7 @@
 import StyledHeader from "containers/Header/Header.styled";
-import { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { getAllPost } from "redux/reducers/postsList";
 
 
 const Welcome = () => {
-  const state = useSelector((state: any)  => state.postsList);
-  const dispatch = useDispatch();
-  
-  useEffect(() => {
-    dispatch(getAllPost())
-  }, [dispatch])
-  console.log(state);
-
   return (
   <StyledHeader />
   )
