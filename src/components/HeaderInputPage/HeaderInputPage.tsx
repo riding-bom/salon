db757a6d469f833f306e93ce7d57a56527a3aa45
younho <nnyyhh9247@gmail.com>
2021-04-13 15:02:49 +0900

    [수정] 헤더의 textInputPage 작성 후 파이어베이스에 연결 테스트
    1. 헤더의 textInputPage 작성 후 파이어베이스에 연결 테스트

--- src/components/HeaderInputPage/HeaderInputPage.tsx
+++ src/components/HeaderInputPage/HeaderInputPage.tsx
@@ -1,9 +1,26 @@
+import InputText from 'components/InputText/InputText';
+import Button from 'components/Button/Button.styled';
+import { useSelector, useDispatch } from 'react-redux';
+import { changeNameAction, changeSalonIntroAction } from 'redux/reducers/salonInfo';
+
 const HeaderInputPage = () => {
+  const salonInfo = useSelector((state: any) => state.salonInfo);
+  const dispatch = useDispatch();
+
+  const onChangeHostName = ({ target }: { target: HTMLInputElement }) => {
+    dispatch(changeNameAction(target.value));
+  };
+  const onChangeSalonIntro = ({ target }: { target: HTMLInputElement }) => {
+    dispatch(changeSalonIntroAction(target.value));
+  };
+
   return (
     <>
-      <input type='text' placeholder='작가님의 이름을 입력해주세요' />
-      <input type='text' placeholder='작가님의 소개글을 입력해주세요' />
-      <button>등록</button>
+      <InputText id='salonInfoHostName' name='작가의 이름' onChange={onChangeHostName} />
+      <InputText id='salonInfoIntro' name='작가의 소개글' onChange={onChangeSalonIntro} />
+      <Button type='submit' width='50'>
+        등록
+      </Button>
     </>
   );
 };
