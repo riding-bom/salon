ca9896974f4882f0f3e18b1c3b746b8ee666312f
younho <nnyyhh9247@gmail.com>
2021-04-13 10:43:58 +0900

    [수정] 헤더 input창 테스트 추가
    1. input창을 생성하여 firebase와 연동하는 테스트 진행

---
+++ src/components/HeaderInputPage/HeaderInputPage.tsx
@@ -0,0 +1,11 @@
+const HeaderInputPage = () => {
+  return (
+    <>
+      <input type='text' placeholder='작가님의 이름을 입력해주세요' />
+      <input type='text' placeholder='작가님의 소개글을 입력해주세요' />
+      <button>등록</button>
+    </>
+  );
+};
+
+export default HeaderInputPage;
