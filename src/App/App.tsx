f1bcf8e4941f6c3f7f1e6f1f5b13e5614a22f686
bg-shorthand <hanhanhan333@gmail.com>
2021-04-21 17:05:39 +0900

    [수정] 리스트 렌더링 순서(최신순), 라우터 스위치

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -2,7 +2,7 @@ import Welcome from "pages/Welcome";
 import StoreProvider from "redux/store";
 import SignIn from "pages/SignIn";
 import SignUp from "pages/SignUp";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import StyledWriteContainer from "containers/WriteContainer/WriteContainer.styled";
 import StyledReadPost from "pages/ReadPost";
 
@@ -10,15 +10,18 @@ function App() {
   return (
     <StoreProvider>
       <Router>
-        <Route path="/" exact>
-          <Welcome />
-        </Route>
-        <Route path="/write">
-          <StyledWriteContainer />
-        </Route>
-        <Route path="/:postId">
-          <StyledReadPost />
-        </Route>
+        <Switch>
+          <Route path="/write">
+            <StyledWriteContainer />
+          </Route>
+          <Route path="/:postId">
+            <StyledReadPost />
+          </Route>
+          <Route path="/" exact>
+            <Welcome />
+          </Route>
+        </Switch>
+        <Link to="/write">쓰기</Link>
 
         <SignIn />
         <SignUp />
