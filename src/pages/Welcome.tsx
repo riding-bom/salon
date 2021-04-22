fd0e06481878355d9d29814196a50a99b02120c1
yhun940731@gmail.com <yhun940731@gmail.com>
2021-04-22 17:37:03 +0900

    [추가] WritePage 필수 입력 사항 경고 modal (solved: #26)

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -2,10 +2,12 @@ import StyledCarousel from "containers/Carousel/Carousel.styled";
 import StyledHeader from "containers/Header/Header.styled";
 import StyledListContainer from "containers/ListContainer/ListContainer.styled";
 import StyledPagination from "containers/Pagination/Pagination.styled";
+import { Link } from "react-router-dom";
 
 const Welcome = () => {
   return (
     <>
+      <Link to="/write">쓰기</Link>
       <StyledHeader />
       <StyledCarousel />
       <StyledListContainer />
