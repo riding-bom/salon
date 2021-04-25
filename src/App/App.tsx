6e18ee218181d624c9aa20c1204699557828c235
bg-shorthand <hanhanhan333@gmail.com>
2021-04-25 16:25:33 +0900

    [수정] App.ts 의 모달 페이지들 정리
    1. ModalDialogs.tsx 를 페이지스 폴더에 만든 후,
    2. 기존 모달 페이지들을 한 데 묶음.
    3. 기존 모달 페이지들은 컨테이너의 ModalDialogs 폴더로 이동.

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,16 +1,10 @@
 import Welcome from "pages/Welcome";
 import StoreProvider from "redux/store";
-import SignIn from "pages/SignIn";
-import SignUp from "pages/SignUp";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import StyledWritePage from "pages/WritePage";
 import StyledReadPost from "pages/ReadPost";
-import AlertDeletePost from "pages/AlertDeletePost";
-import AlertWritePost from "pages/AlertWritePost";
-import AlertCancelWriter from "pages/AlertCancelWriter";
-import NeedSignIn from "pages/NeedSignIn";
-import AlertCancelLike from "pages/AlertCancelLike";
 import LikePosts from "pages/LikePosts";
+import ModalDialogs from "pages/ModalDialogs";
 
 function App() {
   return (
@@ -31,13 +25,7 @@ function App() {
           </Route>
         </Switch>
 
-        <SignIn />
-        <SignUp />
-        <AlertDeletePost />
-        <AlertWritePost />
-        <AlertCancelWriter />
-        <NeedSignIn />
-        <AlertCancelLike />
+        <ModalDialogs />
       </Router>
     </StoreProvider>
   );
